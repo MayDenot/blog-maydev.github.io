a303ebee9a5d4ac0b74635eeb8c3fae8c055dde5
May <mayra.denot@gmail.com>
2023-09-07 16:36:50 -0300

    Agregado el archivo yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://maydenot.github.io/blog-maydev.github.io/",
+  base: "/blog-maydev.github.io",
+});
