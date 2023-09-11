59f2d77a2c3df8e09b9af3021eaedc536f82b76d
May <mayra.denot@gmail.com>
2023-09-11 11:21:29 -0300

    Fix paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://maydenot.github.io/blog-maydev.github.io/",
-  base: "/blog-maydev.github.io",
-});
+export default defineConfig({});
